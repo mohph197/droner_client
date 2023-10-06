3a5a5fcc38513ce791740d2ad763792ec9c54632
Mohammed Tirichine <mohtiri718@gmail.com>
2023-10-06 16:13:07 +0100

    Added Map

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,3 @@
-// // import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-// import "leaflet/dist/leaflet.css";
-// // import Header from "./components/Header";
-// // import Sidebar from "./components/Sidebar";
-// import Dashboard from "./pages/Dashboard";
-
-// function App() {
-//   return (
-//     <>
-//       <Dashboard />
-//       {/* <Header />
-//       <Sidebar /> */}
-//       {/* <MapContainer
-//         className="w-60 h-60"
-//         center={[51.505, -0.09]}
-//         zoom={13}
-//         scrollWheelZoom={false}
-//       >
-//         <TileLayer
-//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         />
-//         <Marker position={[51.505, -0.09]}>
-//           <Popup>
-//             A pretty CSS3 popup. <br /> Easily customizable.
-//           </Popup>
-//         </Marker>
-//       </MapContainer> */}
-//     </>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
--- src/pages/LiveMap.jsx
+++ src/pages/LiveMap.jsx
@@ -1,10 +1,23 @@
 import Sidebar from '../components/Sidebar';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function LiveMap() {
 	return (
 		<div className='flex w-full'>
 			<Sidebar />
-			<h1>Map</h1>
+			<MapContainer
+				className='w-60 h-60'
+				center={[51.505, -0.09]}
+				zoom={13}
+				scrollWheelZoom={false}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+				/>
+				<Marker position={[51.505, -0.09]}></Marker>
+			</MapContainer>
 		</div>
 	);
 }
