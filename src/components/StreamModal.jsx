548b9724a8ef9cbb89c475f57ff3caf498e9e578
wassim <wbvb41@gmail.com>
2023-10-07 08:58:01 +0200

    updated the close stream icon

--- src/components/StreamModal.jsx
+++ src/components/StreamModal.jsx
@@ -1,60 +1,61 @@
-import OvenPlayer from 'ovenplayer';
-import { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { useModalsContext } from '../hooks/useModalsContext';
+import OvenPlayer from "ovenplayer";
+import { useEffect } from "react";
+import PropTypes from "prop-types";
+import { useModalsContext } from "../hooks/useModalsContext";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 const StreamModal = ({ streamURL }) => {
-	useEffect(() => {
-		const player = OvenPlayer.create('player', {
-			autoStart: true,
-			mute: true,
-			controls: false,
-			sources: [
-				{
-					type: 'webrtc',
-					file: streamURL,
-				},
-			],
-			events: {
-				onReady: () => {
-					console.log('Player is ready');
-				},
-				onPlay: () => {
-					console.log('Player is playing');
-				},
-				onPause: () => {
-					console.log('Player is paused');
-				},
-				onEnded: () => {
-					console.log('Player is ended');
-				},
-				onError: (error) => {
-					console.log('Player is error', error);
-				},
-			},
-		});
+  useEffect(() => {
+    const player = OvenPlayer.create("player", {
+      autoStart: true,
+      mute: true,
+      controls: false,
+      sources: [
+        {
+          type: "webrtc",
+          file: streamURL,
+        },
+      ],
+      events: {
+        onReady: () => {
+          console.log("Player is ready");
+        },
+        onPlay: () => {
+          console.log("Player is playing");
+        },
+        onPause: () => {
+          console.log("Player is paused");
+        },
+        onEnded: () => {
+          console.log("Player is ended");
+        },
+        onError: (error) => {
+          console.log("Player is error", error);
+        },
+      },
+    });
 
-		return () => {
-			player.remove();
-		};
-	}, []);
+    return () => {
+      player.remove();
+    };
+  }, []);
 
-	const { closeModal } = useModalsContext();
+  const { closeModal } = useModalsContext();
 
-	return (
-		<div className='relative rounded-xl overflow-hidden h-3/4 aspect-video isolate'>
-			<button
-				className='text-red-500 absolute left-0 top-0 z-10'
-				onClick={closeModal}
-			>
-				CLOSE
-			</button>
-			<div id='player'></div>
-		</div>
-	);
+  return (
+    <div className="relative rounded-xl overflow-hidden h-3/4 aspect-video isolate">
+      <button
+        className="text-red-500 absolute right-2 top-2 z-10"
+        onClick={closeModal}
+      >
+        <AiFillCloseCircle className="text-3xl text-white" />
+      </button>
+      <div id="player"></div>
+    </div>
+  );
 };
 StreamModal.propTypes = {
-	streamURL: PropTypes.string.isRequired,
+  streamURL: PropTypes.string.isRequired,
 };
 
 export default StreamModal;
