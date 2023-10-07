65d76fd2e24ff8a0959f9310f5f04385e333b9e3
wassim <wbvb41@gmail.com>
2023-10-07 04:55:37 +0200

    removed z-index from livemap

--- src/pages/LiveMap.jsx
+++ src/pages/LiveMap.jsx
@@ -1,42 +1,42 @@
-import Sidebar from '../components/Sidebar';
+import Sidebar from "../components/Sidebar";
 import {
-	MapContainer,
-	TileLayer,
-	Marker,
-	useMap,
-	useMapEvents,
-} from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import DronesMarkers from '../components/DronesMarkers';
+  MapContainer,
+  TileLayer,
+  Marker,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import DronesMarkers from "../components/DronesMarkers";
 
 const LiveMap = () => {
-	const UpdateMap = () => {
-		const map = useMap();
+  const UpdateMap = () => {
+    const map = useMap();
 
-		useMapEvents({
-			click: (event) => console.log(event.latlng),
-		});
+    useMapEvents({
+      click: (event) => console.log(event.latlng),
+    });
 
-		return null;
-	};
+    return null;
+  };
 
-	return (
-		<div className='flex w-full h-full'>
-			<Sidebar />
-			<MapContainer
-				className='w-full h-full isolate -z-10'
-				center={[51.505, -0.09]}
-				zoom={13}
-			>
-				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-				/>
-				<UpdateMap />
-				<DronesMarkers />
-			</MapContainer>
-		</div>
-	);
+  return (
+    <div className="flex w-full h-full">
+      <Sidebar />
+      <MapContainer
+        className="w-full h-full isolate"
+        center={[51.505, -0.09]}
+        zoom={13}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <UpdateMap />
+        <DronesMarkers />
+      </MapContainer>
+    </div>
+  );
 };
 
 export default LiveMap;
