980b932a66975a182606d8f97ea7cdcf71f7cdd7
Mohammed Tirichine <mohtiri718@gmail.com>
2023-10-07 03:15:19 +0100

    Added drone live stream

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import LiveMap from './pages/LiveMap';
 import { SelectedDroneProvider } from './context/SelectedDroneContext';
+import { useModalsContext } from './hooks/useModalsContext';
 
 const App = () => {
+	const { CurrentModal, closeModal } = useModalsContext();
+
 	return (
 		<BrowserRouter>
-			<div className='flex flex-col w-screen h-screen overflow-hidden'>
+			<div className='flex flex-col w-screen h-screen overflow-hidden isolate'>
 				<Header />
 				<div className='flex-1'>
 					<Routes>
@@ -23,6 +26,15 @@ const App = () => {
 						<Route path='/settings' element={<Settings />} />
 					</Routes>
 				</div>
+				{CurrentModal && (
+					<div className='fixed inset-0 z-50 flex items-center justify-center isolate'>
+						<div
+							className='absolute inset-0 bg-black bg-opacity-40 backdrop-blur -z-10 hover:cursor-pointer'
+							onClick={closeModal}
+						></div>
+						<CurrentModal.Modal {...CurrentModal.args} />
+					</div>
+				)}
 			</div>
 		</BrowserRouter>
 	);
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { ModalsProvider } from './context/ModalsContext.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<ModalsProvider>
+		<App />
+	</ModalsProvider>
+);
